84ee3bcbcb61b5d6f33dea47933269332a54c313
Mason Veilleux <masonjveilleux@gmail.com>
2024-12-18 14:09:35 -0800

    typos

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   experimental: {
     reactCompiler: true,
   },
-  pageExtensions: ['mdx', 'tsx','ts'],
+  pageExtensions: ['mdx', 'tsx', 'ts'],
 };
 
 module.exports = withMDX(nextConfig);
