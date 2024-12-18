4be7f29c57664c14c069068288ba917251ce12ae
Mason Veilleux <masonjveilleux@gmail.com>
2024-12-18 13:35:28 -0800

    greatly simplying the mobile numeric keyboard changes lol

--- app/components/calculator/input.tsx
+++ app/components/calculator/input.tsx
@@ -1,65 +1,29 @@
-import { forwardRef, useEffect, useState } from "react";
+import { forwardRef } from "react";
 import { xMax, xMin } from "../../lib/calculate";
 import { Input as ShadInput } from "../ui/input";
-
-const useIsMobile = () => {
-  const [isMobile, setIsMobile] = useState<boolean>(false);
-  
-  useEffect(() => {
-    const checkIsMobile = (): boolean => {
-      if (typeof window === 'undefined') return false;
-      return /iPhone|iPad/i.test(navigator.userAgent);
-    };
-    setIsMobile(checkIsMobile());
-  }, []);
-  
-  return isMobile;
-};
-
-interface InputProps {
-  value: number;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
-const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  const { value, onChange } = props;
-  const isMobile = useIsMobile();
-  
-  if (isMobile) {
-    return (
-      <div className="relative w-48">
-        <ShadInput
-          type="number"
-          inputMode="numeric"
-          pattern="[0-9]*"
-          placeholder="Distance"
-          value={value}
-          onChange={onChange}
-          aria-label="Distance"
-          min={xMin}
-          max={xMax}
-          ref={ref}
-          className="w-full"
-        />
-      </div>
-    );
+const Input = forwardRef<
+  HTMLInputElement,
+  {
+    value: number;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   }
-
+>(({ value, onChange }, ref) => {
   return (
-    <ShadInput
-      type="number"
-      placeholder="Distance"
-      value={value}
-      onChange={onChange}
-      aria-label="Distance"
-      min={xMin}
-      max={xMax}
-      ref={ref}
-      className="w-48"
-    />
+    <div className="relative w-full mb-3">
+      <ShadInput
+        type="number"
+        placeholder="Distance"
+        inputMode="numeric"
+        pattern="[0-9]*"
+        value={value}
+        onChange={onChange}
+        aria-label="Distance"
+        min={xMin}
+        max={xMax}
+        ref={ref}
+      />
+    </div>
   );
 });
-
-Input.displayName = "Input";
 
 export default Input;
